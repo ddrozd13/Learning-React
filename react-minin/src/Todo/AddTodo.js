c05a582cca26727e47afe1fb9c21cf370b34fadd
Dima Drozd <ziger2154@gmail.com>
2022-01-21 20:55:12 +0300

    Create react-app with Minin

---
+++ react-minin/src/Todo/AddTodo.js
@@ -0,0 +1,41 @@
+import propTypes from "prop-types";
+import React from "react";
+
+function useInputValue(defaultValue = '') {
+  const [value, setValue] = React.useState('');
+
+  return {
+    bind: {
+      value,
+      onChange: event => setValue(event.target.value)
+    },
+    clear: () => setValue(''),
+    value: () => value,
+  }
+}
+
+function AddTodo({ onCreate }) {
+  const input = useInputValue('');
+
+  function submitHandler(event) {
+    event.preventDefault()
+
+    if (input.value().trim()) {
+      onCreate(input.value());
+      input.clear();
+    };
+  };
+
+  return (
+    <form style={{ marginBottom: '1rem'}} onSubmit={submitHandler}>
+      <input {...input.bind} />
+      <button type="submit">Add Todo</button>
+    </form>
+  )
+}
+
+AddTodo.propTypes = {
+  onCreate: propTypes.func.isRequired
+}
+
+export default AddTodo;
